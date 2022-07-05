04fd401bc843493022100b0d1c7ad5db3acbf142
Mariya Tumelya <mtumelia@gmail.com>
2022-07-05 19:35:29 +0300

    feat: add avatar with message

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Message from "./Message";
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    //avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    avatar: "https://wallpapers.com/images/featured/Cute%20Anime-uptxxcxl4h2zoe9r.jpg",
+    name: 'Maria',
+    message: 'yarn start нажимал?',
+    time: '22:00'
 }
 
 function HW1() {
@@ -15,12 +17,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import style from './Message.module.css';
 
-function Message() {
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={style.common}>
+            <img alt={"avatar"} src={props.avatar}/>
+            <div className={style.triangle}></div>
+            <div className={style.messageForm}>
+                <div className={style.left}>
+                    <p className={style.name}>{props.name}</p>
+                    <p className={style.mess}>{props.message}</p>
+                </div>
+                <div className={style.right}>
+                    <p className={style.time}>{props.time}</p>
+                </div>
+            </div>
         </div>
     )
 }
